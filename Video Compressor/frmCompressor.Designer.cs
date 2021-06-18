a2290846b2c471714542963aa1fbc9492979d8ab
Shrikant Dandile <ShrikantDandile@github.com>
2021-06-19 00:32:56 +0530

    Add project files.

---
+++ Video Compressor/frmCompressor.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Video_Compressor
+{
+   partial class Compressor
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+         this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.btnBrowse = new System.Windows.Forms.Button();
+         this.label1 = new System.Windows.Forms.Label();
+         this.btnBrowseOutputPath = new System.Windows.Forms.Button();
+         this.btnCompress = new System.Windows.Forms.Button();
+         this.textBox3 = new System.Windows.Forms.TextBox();
+         this.textBox4 = new System.Windows.Forms.TextBox();
+         this.textBox5 = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // openFileDialog1
+         // 
+         this.openFileDialog1.FileName = "openFileDialog1";
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point(136, 15);
+         this.textBox1.Multiline = true;
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(779, 56);
+         this.textBox1.TabIndex = 0;
+         // 
+         // btnBrowse
+         // 
+         this.btnBrowse.Location = new System.Drawing.Point(913, 14);
+         this.btnBrowse.Name = "btnBrowse";
+         this.btnBrowse.Size = new System.Drawing.Size(94, 57);
+         this.btnBrowse.TabIndex = 1;
+         this.btnBrowse.Text = "Browse";
+         this.btnBrowse.UseVisualStyleBackColor = true;
+         this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 32);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(102, 20);
+         this.label1.TabIndex = 2;
+         this.label1.Text = "Input File Path";
+         // 
+         // btnBrowseOutputPath
+         // 
+         this.btnBrowseOutputPath.Location = new System.Drawing.Point(555, 89);
+         this.btnBrowseOutputPath.Name = "btnBrowseOutputPath";
+         this.btnBrowseOutputPath.Size = new System.Drawing.Size(231, 56);
+         this.btnBrowseOutputPath.TabIndex = 4;
+         this.btnBrowseOutputPath.Text = "Open";
+         this.btnBrowseOutputPath.UseVisualStyleBackColor = true;
+         this.btnBrowseOutputPath.Click += new System.EventHandler(this.btnBrowseOutputPath_Click);
+         // 
+         // btnCompress
+         // 
+         this.btnCompress.Location = new System.Drawing.Point(270, 89);
+         this.btnCompress.Name = "btnCompress";
+         this.btnCompress.Size = new System.Drawing.Size(231, 56);
+         this.btnCompress.TabIndex = 6;
+         this.btnCompress.Text = "Compress Video";
+         this.btnCompress.UseVisualStyleBackColor = true;
+         this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+         // 
+         // textBox3
+         // 
+         this.textBox3.Location = new System.Drawing.Point(12, 175);
+         this.textBox3.Multiline = true;
+         this.textBox3.Name = "textBox3";
+         this.textBox3.Size = new System.Drawing.Size(995, 175);
+         this.textBox3.TabIndex = 7;
+         // 
+         // textBox4
+         // 
+         this.textBox4.Location = new System.Drawing.Point(12, 356);
+         this.textBox4.Multiline = true;
+         this.textBox4.Name = "textBox4";
+         this.textBox4.Size = new System.Drawing.Size(409, 105);
+         this.textBox4.TabIndex = 8;
+         // 
+         // textBox5
+         // 
+         this.textBox5.Location = new System.Drawing.Point(427, 356);
+         this.textBox5.Multiline = true;
+         this.textBox5.Name = "textBox5";
+         this.textBox5.Size = new System.Drawing.Size(580, 105);
+         this.textBox5.TabIndex = 9;
+         // 
+         // Compressor
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(1019, 471);
+         this.Controls.Add(this.textBox5);
+         this.Controls.Add(this.textBox4);
+         this.Controls.Add(this.textBox3);
+         this.Controls.Add(this.btnCompress);
+         this.Controls.Add(this.btnBrowseOutputPath);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.btnBrowse);
+         this.Controls.Add(this.textBox1);
+         this.ForeColor = System.Drawing.SystemColors.Highlight;
+         this.Name = "Compressor";
+         this.Text = "Compressor";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.OpenFileDialog openFileDialog1;
+      private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Button btnBrowse;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Button btnBrowseOutputPath;
+      private System.Windows.Forms.Button btnCompress;
+      private System.Windows.Forms.TextBox textBox3;
+      private System.Windows.Forms.TextBox textBox4;
+      private System.Windows.Forms.TextBox textBox5;
+   }
+}
+
---
+++ Video Compressor/frmCompressor.cs
@@ -0,0 +1,193 @@
+﻿using FFmpeg.NET;
+using FFmpeg.NET.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Video_Compressor
+{
+   public partial class Compressor : Form
+   {
+      public Compressor()
+      {
+         InitializeComponent();
+         if (!Directory.Exists(dicrectoryPath))
+         {
+            Directory.CreateDirectory(dicrectoryPath);
+         }
+      }
+
+      readonly static string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+      readonly static string outputFolderName = "Compressor\\Output\\";
+      readonly static string dicrectoryPath = Path.Combine(documentsPath, outputFolderName);
+      private void Compressor_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void btnBrowse_Click(object sender, EventArgs e)
+      {
+         OpenFileDialog openFileDialog1 = new OpenFileDialog
+         {
+            InitialDirectory = Path.Combine(documentsPath, @"\Compressor\Input"),
+            Title = "Browse Video Files",
+
+            CheckFileExists = true,
+            CheckPathExists = true,
+
+            DefaultExt = "mp4",
+            Filter = "mp4 files (*.mp4)|*.mp4|All files (*.*)|*.*",
+            FilterIndex = 2,
+            RestoreDirectory = true,
+
+            ReadOnlyChecked = true,
+            ShowReadOnly = true
+         };
+
+         if (openFileDialog1.ShowDialog() == DialogResult.OK)
+         {
+            textBox1.Text = openFileDialog1.FileName;
+         }
+      }
+
+      private void btnCompress_Click(object sender, EventArgs e)
+      {
+         var inputFile = textBox1.Text;
+         if (string.IsNullOrEmpty(textBox1.Text))
+         {
+            textBox3.Text = "Please Browse Video!";
+            return;
+         }
+         var outputFile = dicrectoryPath + Guid.NewGuid().ToString() + ".mp4";
+         //  var mediaInfo = FFProbe.Analyse(inputFile);
+         try
+         {
+            Task.Run(() => StartConverting(inputFile, outputFile));
+         }
+         catch (Exception ex)
+         {
+            textBox3.Text = ex.Message;
+         }
+
+      }
+      public async Task StartConverting(string inputFileLocation, string outputFileLocation)
+      {
+         var inputFile = new MediaFile(inputFileLocation);
+         var outputFile = new MediaFile(outputFileLocation);
+         string directory = AppDomain.CurrentDomain.BaseDirectory + "ffmpeg.exe";
+         var ffmpeg = new Engine(directory);
+         ffmpeg.Progress += OnProgress;
+         ffmpeg.Data += OnData;
+         ffmpeg.Error += OnError;
+         ffmpeg.Complete += OnComplete;
+         await ffmpeg.ConvertAsync(inputFile, outputFile);
+      }
+      delegate void SetTextCallback(string text);
+      delegate void SetDataTextCallback(string text);
+      delegate void SetCompetedTextCallback(string text);
+
+      private void SetText(string text)
+      {
+         // InvokeRequired required compares the thread ID of the
+         // calling thread to the thread ID of the creating thread.
+         // If these threads are different, it returns true.
+         if (this.textBox3.InvokeRequired)
+         {
+            SetTextCallback d = new SetTextCallback(SetText);
+            this.Invoke(d, new object[] { text });
+         }
+         else
+         {
+            this.textBox3.Text = text + Environment.NewLine;
+         }
+      }
+      private void SetDataText(string text)
+      {
+         // InvokeRequired required compares the thread ID of the
+         // calling thread to the thread ID of the creating thread.
+         // If these threads are different, it returns true.
+         if (this.textBox4.InvokeRequired)
+         {
+            SetDataTextCallback d = new SetDataTextCallback(SetDataText);
+            this.Invoke(d, new object[] { text });
+         }
+         else
+         {
+            this.textBox4.Text = text + Environment.NewLine;
+         }
+      }
+
+
+      private void OnProgress(object sender, ConversionProgressEventArgs e)
+      {
+
+         var finalString = string.Format("[{0} => {1}]"
+              + Environment.NewLine + "Bitrate: {2}"
+              + Environment.NewLine + "Fps: {3}"
+              + Environment.NewLine + "Frame: {4}"
+              + Environment.NewLine + "ProcessedDuration: {5}"
+              + Environment.NewLine + "Size: {6} kb"
+              + Environment.NewLine + "TotalDuration: {7} "
+              + Environment.NewLine + "Percentage: {8}%",
+              e.Input.FileInfo.Name,
+              e.Output.FileInfo.Name,
+              e.Bitrate,
+              e.Fps,
+              e.Frame,
+              e.ProcessedDuration,
+              e.SizeKb,
+               e.TotalDuration,
+               Math.Round((((double)e.ProcessedDuration.Ticks / (double)e.TotalDuration.Ticks)*100),2,MidpointRounding.ToZero)
+              );
+         SetText(finalString);
+      }
+
+      private void OnData(object sender, ConversionDataEventArgs e)
+      {
+         SetDataText(string.Format("[{0} => {1}]: {2}", e.Input.FileInfo.Name, e.Output.FileInfo.Name, e.Data));
+      }
+
+      private void OnComplete(object sender, ConversionCompleteEventArgs e)
+      {
+         SetCompetedText(string.Format("Completed conversion from {0} to {1}", e.Input.FileInfo.FullName, e.Output.FileInfo.FullName));
+      }
+
+      private void SetCompetedText(string text)
+      {
+         // InvokeRequired required compares the thread ID of the
+         // calling thread to the thread ID of the creating thread.
+         // If these threads are different, it returns true.
+         if (this.textBox5.InvokeRequired)
+         {
+            SetCompetedTextCallback d = new SetCompetedTextCallback(SetCompetedText);
+            this.Invoke(d, new object[] { text });
+         }
+         else
+         {
+            this.textBox5.Text = text + Environment.NewLine;
+         }
+      }
+
+      private void OnError(object sender, ConversionErrorEventArgs e)
+      {
+         SetCompetedText(string.Format("[{0} => {1}]: Error: {2}\n{3}", e.Input.FileInfo.Name, e.Output.FileInfo.Name, e.Exception.ExitCode, e.Exception.InnerException));
+      }
+
+      private void btnBrowseOutputPath_Click(object sender, EventArgs e)
+      {
+         System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+         {
+            FileName = dicrectoryPath,
+            UseShellExecute = true,
+            Verb = "open"
+         });
+      }
+   }
+}
